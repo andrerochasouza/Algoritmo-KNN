3ed697f4c70e27e6eb3a67c53c38ae5a05027f12
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-05-06 16:12:01 -0300

    nova classe para os métodos gerais

--- src/App.java
+++ src/App.java
@@ -1,9 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 public class App {
 
@@ -22,6 +17,8 @@ public class App {
          * 
          */
 
+        ArrayList<int[]> dataset = new ArrayList<>();
+
         int a[] = { 1, 1, 3, 4, 5, 1 };
         int b[] = { 2, 1, 4, 5, 5, 1 };
         int c[] = { 1, 2, 5, 4, 3, 1 };
@@ -30,88 +27,17 @@ public class App {
         int f[] = { 4, 5, 1, 1, 2, 0 };
         int g[] = { 5, 5, 1, 1, 1, 0 };
 
-        ArrayList<int[]> dataset = new ArrayList<>();
+        dataset.add(a);
+        dataset.add(b);
+        dataset.add(c);
+        dataset.add(d);
+        dataset.add(e);
+        dataset.add(f);
+        dataset.add(g);
 
         int[] amostra = { 3, 3, 2, 1, 1 };
 
-        classificadorKNN(3, dataset, amostra);
+        MetodosKNN.classificadorKNN(3, dataset, amostra);
 
-    }
-
-    static ArrayList<Double> calcularDataset(ArrayList<int[]> dataset, int[] amostra) {
-
-        ArrayList<Double> calculoDatasetEuclidiano = new ArrayList<>();
-
-        for (int i = 0; i < dataset.size(); i++) {
-
-            int[] treinamentoPosicao = new int[dataset.size() - 1];
-            for (int j = 0; j < (dataset.size() - 1); j++) {
-                treinamentoPosicao[j] = dataset.get(i)[j];
-            }
-
-            System.out.println("Indice -- " + i + " | Valores euclidianos -- "
-                    + distanciaEuclidiana(amostra, treinamentoPosicao));
-
-            calculoDatasetEuclidiano.add(distanciaEuclidiana(amostra, treinamentoPosicao));
-        }
-
-        System.out.println("------------------------------");
-        return calculoDatasetEuclidiano;
-    }
-
-    static void classificadorKNN(int k, ArrayList<int[]> dataset, int[] amostra) {
-
-        // Acoplando o indice no calculo euclidiano
-        LinkedHashMap<Integer, Double> map = new LinkedHashMap<>();
-        for (int i = 0; i < ordenacaoPorDistancia.size(); i++) {
-            map.put(i, ordenacaoPorDistancia.get(i));
-        }
-
-        // Printando o mapa de forma ordenada
-        map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEachOrdered(x -> System.out.println(x.getKey() + " - " + x.getValue()));
-
-        System.out.println("------------------------------");
-
-        // Pegando os indices dos k primeiros e coletando os tipos de personagem
-        map = map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .limit(k)
-                .collect(LinkedHashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
-
-        int sumGuerreiro = 0;
-        int sumMago = 0;
-
-        for (Map.Entry<Integer, Double> entry : map.entrySet()) {
-
-            System.out.println("Indice -- " + entry.getKey() + " | Valor -- " + entry.getValue() + " | Personagem -- "
-                    + dataset.get(entry.getKey()).getTipoPersonagemEnum());
-
-            if (dataset.get(entry.getKey()).tipoPersonagemEnum == TipoPersonagemEnum.GUERREIRO) {
-                sumGuerreiro++;
-            } else {
-                sumMago++;
-            }
-        }
-
-        System.out.println("------------------------------");
-
-        if (sumGuerreiro > sumMago) {
-            System.out.println("Amostra: Guerreiro");
-        } else {
-            System.out.println("Amostra: Mago");
-        }
-    }
-
-    static double distanciaEuclidiana(int[] p1, int[] p2) {
-
-        double bd = 0.0;
-
-        for (int i = 0; i < p2.length; i++) {
-            bd = Math.pow((p1[i] - p2[i]), 2) + bd;
-        }
-
-        return Math.sqrt(bd);
     }
 }
---
+++ src/MetodosKNN.java
@@ -0,0 +1,152 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * Criado por André da Rocha Souza on 5/5/2022.
+ * 
+ * Email - andredarochasouza.12345@gmail.com
+ * 
+ * @Author andre.rsouza
+ * 
+ */
+
+public class MetodosKNN {
+
+    /**
+     * 
+     * Método estático que carrega os dados do arquivo CSV separados com VIRGULA (,)
+     * e
+     * retorna uma lista de frases do tipo array de strings.
+     * 
+     * @Author andre.rsouza
+     * 
+     */
+
+    static List<String[]> loadDataset(String pathArquive) throws Exception {
+
+        if (pathArquive == null) {
+            try {
+                BufferedReader arquive = new BufferedReader(new FileReader(pathArquive));
+                List<String[]> arq = new ArrayList<>();
+
+                arq.stream().forEach(line -> {
+                    try {
+                        arquive.readLine().split(",");
+                    } catch (IOException e) {
+                        System.out.println("Erro ao ler a linha do arquivo, Erro: " + e.getMessage());
+                    }
+                });
+
+                arquive.close();
+                return arq;
+
+            } catch (Exception e) {
+                System.out.println("Erro ao abrir o arquivo no caminho especificado: " + pathArquive);
+            }
+
+        } else {
+            throw new Exception("Path arquivo não pode ser nulo");
+        }
+        return null;
+
+    }
+
+    static ArrayList<Double> calcularDataset(ArrayList<int[]> dataset, int[] amostra) {
+
+        ArrayList<Double> calculoDatasetEuclidiano = new ArrayList<>();
+
+        for (int i = 0; i < dataset.size(); i++) {
+
+            int[] treinamentoPosicao = new int[amostra.length];
+            for (int j = 0; j < amostra.length; j++) {
+                treinamentoPosicao[j] = dataset.get(i)[j];
+            }
+
+            System.out.println("Indice -- " + i + " | Valores euclidianos -- "
+                    + distanciaEuclidiana(amostra, treinamentoPosicao));
+
+            calculoDatasetEuclidiano.add(distanciaEuclidiana(amostra, treinamentoPosicao));
+        }
+
+        System.out.println("------------------------------");
+        return calculoDatasetEuclidiano;
+    }
+
+    static HashMap<Integer, Double> mapeandoIndicesEuclidians(ArrayList<Double> arrCalculosEuclidianos) {
+
+        // Acoplando o indice no calculo euclidiano
+        HashMap<Integer, Double> map = new HashMap<>();
+        for (int i = 0; i < arrCalculosEuclidianos.size(); i++) {
+            map.put(i, arrCalculosEuclidianos.get(i));
+        }
+
+        // Printando o mapa de forma ordenada
+        map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(x -> System.out.println(x.getKey() + " - " + x.getValue()));
+
+        System.out.println("------------------------------");
+
+        return map;
+    }
+
+    static void classificadorKNN(int k, ArrayList<int[]> dataset, int[] amostra) {
+
+        HashMap<Integer, Double> map = mapeandoIndicesEuclidians(calcularDataset(dataset, amostra));
+
+        // Pegando os indices dos k primeiros e coletando os tipos de personagem
+        map = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .limit(k)
+                .collect(HashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+
+        int sumGuerreiro = 0;
+        int sumMago = 0;
+
+        for (Map.Entry<Integer, Double> entry : map.entrySet()) {
+
+            System.out.println("Indice -- " + entry.getKey() + " | Valor -- " + entry.getValue() + " | Personagem -- "
+                    + dataset.get(entry.getKey())[5]);
+
+            if (dataset.get(entry.getKey())[5] == 0) {
+                sumGuerreiro++;
+            } else {
+                sumMago++;
+            }
+        }
+
+        System.out.println("------------------------------");
+
+        if (sumGuerreiro > sumMago) {
+            System.out.println("Amostra: Guerreiro");
+        } else {
+            System.out.println("Amostra: Mago");
+        }
+    }
+
+    /**
+     * 
+     * Método estático que calcula a distância euclidiana entre duas posições
+     * de arrays de doubles.
+     * 
+     * @Author andre.rsouza
+     * 
+     */
+
+    static double distanciaEuclidiana(int[] p1, int[] p2) {
+
+        double bd = 0.0;
+
+        for (int i = 0; i < p2.length; i++) {
+            bd = Math.pow((p1[i] - p2[i]), 2) + bd;
+        }
+
+        return Math.sqrt(bd);
+    }
+}
